a6ebae2c808a4da92112bcc14ad48350fdbafba3
Branden Leong <branden.leong.1@gmail.com>
2025-01-05 13:04:52 -0800

    Moved changeTheme to init.js

--- init.js
+++ init.js
@@ -116,6 +116,25 @@ function initTheme(id = 0) {
 	Cookies.setCookie('themeID', id, 5 * 365 * 24 * 60 * 60 * 1000);
 }
 
+async function changeTheme() {
+	let btn = document.querySelector('#theme-btn');
+	btn.onclick = null;
+	btn.themeId = (btn.themeId + 1) % Themes.themes.length;
+
+	let labels = btn.themeLabel1Shown ? [document.querySelector('#theme-label-1'), document.querySelector('#theme-label-2')] : [document.querySelector('#theme-label-2'), document.querySelector('#theme-label-1')];
+	labels[1].innerText = Themes.themes[btn.themeId][0];
+	await Animate.animateGroup([
+		[labels[0], Animate.fadeOut, {shiftTo: UP}],
+		[labels[1], Animate.fadeIn, {shiftFrom: DOWN}]
+	]);
+	document.querySelector('#theme-css').setAttribute('href', Themes.themes[btn.themeId][1]);
+	Cookies.setCookie('themeID', btn.themeId, 5 * 365 * 24 * 60 * 60 * 1000);
+
+	btn.themeLabel1Shown = !btn.themeLabel1Shown;
+	await Animate.wait(1500);
+	btn.onclick = changeTheme;
+}
+
 window.addEventListener('load', async function() {
 	await Utils.sleep(100);
 	document.querySelector('#loading-css').remove();
--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-async function changeTheme() {
-	let btn = document.querySelector('#theme-btn');
-	btn.onclick = null;
-	btn.themeId = (btn.themeId + 1) % Themes.themes.length;
-
-	let labels = btn.themeLabel1Shown ? [document.querySelector('#theme-label-1'), document.querySelector('#theme-label-2')] : [document.querySelector('#theme-label-2'), document.querySelector('#theme-label-1')];
-	labels[1].innerText = Themes.themes[btn.themeId][0];
-	await Animate.animateGroup([
-		[labels[0], Animate.fadeOut, {shiftTo: UP}],
-		[labels[1], Animate.fadeIn, {shiftFrom: DOWN}]
-	]);
-	document.querySelector('#theme-css').setAttribute('href', Themes.themes[btn.themeId][1]);
-	Cookies.setCookie('themeID', btn.themeId, 5 * 365 * 24 * 60 * 60 * 1000);
-
-	btn.themeLabel1Shown = !btn.themeLabel1Shown;
-	await Animate.wait(1500);
-	btn.onclick = changeTheme;
-}
-
 function loadImage() {
 	let files = document.querySelector('#input-image').files;
 	if (files.length) {
